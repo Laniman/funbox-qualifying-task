d8b84eda7323ca2b2b48474fea9e87b3e6c13cdd
George <georgij-net@yandex.ru>
2017-03-10 21:21:09 +0300

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+import svg4everybody from 'svg4everybody';
+import $ from 'jquery';
+
+$(() => {
+	svg4everybody();
+});
+
+const cardClass = '.card';
+const productClass = '.product';
+const selectedHoverClass = 'card_selected_hover';
+
+const products = document.querySelectorAll(productClass);
+
+function mouseleaveEventHandler() {
+	this.classList.remove(selectedHoverClass);
+}
+
+[...products].forEach(product => {
+	const card = product.querySelector(cardClass);
+	const input = product.querySelector('input');
+	card.addEventListener('mouseenter', function () {
+		if (!input.checked) {
+			return;
+		}
+		this.classList.add(selectedHoverClass);
+	});
+	card.addEventListener('mouseleave', mouseleaveEventHandler);
+	input.addEventListener('change', () => {
+		card.classList.remove(selectedHoverClass);
+	});
+});
